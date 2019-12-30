9aa8ad475e0716e44f73b7b410c3b486c73e98cc
Pedro Amaral <pedropaulo@life.com.br>
2019-12-30 16:26:38 -0300

    Acl inline

--- app/Controllers/Http/PostController.js
+++ app/Controllers/Http/PostController.js
@@ -3,8 +3,15 @@
 const Post = use('App/Models/Post')
 
 class PostController {
-  async index ({ request }) {
-    const posts = await Post.all()
+  async index ({ request, auth }) {
+
+    const user = await auth.getUser()
+
+    if(await user.can('read_private_posts')){
+      const posts = await Post.all()
+      return posts
+    }
+    const posts = await Post.query().where({ type: 'public'}).fetch()
 
     return posts
   }
@@ -17,10 +24,25 @@ class PostController {
     return post
   }
 
-  async show ({ params }) {
+  async show ({ params, auth, response }) {
     const post = await Post.findOrFail(params.id)
 
-    return post
+    if(post.type === "public") {
+      return post
+    }
+
+    const user = await auth.getUser()
+
+    if(await user.can('read_private_posts')){
+      return post
+    }
+
+    return response.status(400).send({
+      error: {
+        message: "VocÊ não tem permissão de leitura"
+      }
+    })
+
   }
 
   async update ({ params, request, response }) {
--- start/routes.js
+++ start/routes.js
@@ -26,7 +26,8 @@ Route.resource('/posts', 'PostController')
   .middleware(['auth', 'is:(administrator || moderator || suporte || nada)'])
 
 Route.get('/posts', 'PostController.index')
-  .middleware(['auth', 'can:read_posts'])
+  .middleware(['auth',
+               'can:read_posts || read_private_posts'])
 
 Route.get('/posts/:id', 'PostController.show')
   .middleware(['auth', 'can:read_posts'])
